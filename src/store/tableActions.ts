8933204d530e5d2b1668d2593d3ef3656ea4fc7b
fabien <fabien.andrian@gmail.com>
2022-06-26 13:10:52 +0200

    refacto sort table action

--- src/store/tableActions.ts
+++ src/store/tableActions.ts
@@ -7,34 +7,59 @@ export const setInitialDataAction: IsetInitialDataAction = (table) => {
 
 export const sortTableAction: IsortTableAction = (table, sortByColumn, dataType, isAscending) => {
     
-    const sortedTable = [...table].sort((a, b) => {
+    const getItemsToCompare = (a: TtableRow, b: TtableRow) => {
+        const item1 = a[sortByColumn] || null
+        const item2 = b[sortByColumn] || null
+        return [item1, item2]
+    }
 
-        const var1 = a[sortByColumn] || null
-        const var2 = b[sortByColumn] || null
+    const compareNullValues = (a: Tdata, b: Tdata) => {
+        // null values are sent at the end of the table
+        return !b ? -1 : 1  
+    }
 
-        if (!var1 || !var2) {
-            return !var2 ? -1 : 1 
-        }
+    const numericComparison = (a: number, b: number) => {
+        const comparator = a < b ? 1 : -1
+        return isAscending ? comparator : comparator * (-1)
+    }
 
-        if (dataType === 'number') {
-            const comparator = var1 < var2 ? 1 : -1
-            return isAscending ? comparator :  comparator * (-1)
-        }
+    const dateComparison = (a: string, b: string) => {
+        const comparator = new Date(a) > new Date(b) ? 1 : -1
+        return isAscending ? comparator : comparator * (-1)
+    }
 
-        if (dataType === 'date') {
-            const comparator = new Date(var1) > new Date(var2) ? 1 : -1
-            return isAscending ? comparator : comparator * (-1)
-        }
+    const stringComparison = (a: string, b: string) => {
+        return isAscending ? a.localeCompare(b) : b.localeCompare(a)
+    }
 
-        if (dataType === 'string') {
-            const str1 = var1 as string
-            const str2 = var2 as string
-            return isAscending ? str1.localeCompare(str2) : str2.localeCompare(str1)
-        }
+    let sortedTable: Ttable
 
-        return 1
-        
-    })
+    switch (dataType) {
+        case 'number':
+            sortedTable = [...table].sort((a, b) => {
+                const [item1, item2] = getItemsToCompare(a, b)
+                if (!item1 || !item2) return compareNullValues(item1, item2)
+                return numericComparison(item1 as number, item2 as number)
+            })
+            break
+        case 'date':
+            sortedTable = [...table].sort((a, b) => {
+                const [item1, item2] = getItemsToCompare(a, b)
+                if (!item1 || !item2) return compareNullValues(item1, item2)
+                return dateComparison(item1 as string, item2 as string)
+            })
+            break
+        case 'string':
+            sortedTable = [...table].sort((a, b) => {
+                const [item1, item2] = getItemsToCompare(a, b)
+                if (!item1 || !item2) return compareNullValues(item1, item2)
+                return stringComparison(item1 as string, item2 as string)
+            })
+            break
+        default:
+            sortedTable = [...table]
+            break
+    }
 
     return {
         type: 'SORT_TABLE',
